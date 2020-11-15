61b1be3ac608659f94cbaa4431b2b7bb60c742fc
Thomas Stollenwerk <tstollenwerk@stollenwerk-itconsulting.com>
2020-11-15 20:26:43 +0100

    refactor - unity helper interfaces and classes

---
+++ src/Util/Base64Url.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Base64Url.cs" company="Thomas Stollenwerk (motmot80)">
+// Copyright (c) Thomas Stollenwerk (motmot80). All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Assets.Abune.Client.Unity.Shared.Util
+{
+    using System;
+
+    /// <summary>
+    /// Helper class to support base 64 url encoding
+    /// </summary>
+    public static class Base64Url
+    {
+        /// <summary>
+        /// Transforms byte array to a base 64 url encoded string.
+        /// </summary>
+        /// <param name="data">The input data.</param>
+        /// <returns>Base 64 url encoded data.</returns>
+        public static string Encode(byte[] data)
+        {
+            var output = Convert.ToBase64String(data);
+            return RemoveTrailingChars(output).Replace('+', '-').Replace('/', '_');
+        }
+
+        /// <summary>
+        /// Transforms base 64 url encoded string to byte array.
+        /// </summary>
+        /// <param name="base64UrlData">The base 64 url encoded data.</param>
+        /// <returns>The byte array.</returns>
+        public static byte[] Decode(string base64UrlData)
+        {
+            var reverted = base64UrlData.Replace('-', '+').Replace('_', '/');
+            string b64data = AppendTrailingChars(reverted);
+            return Convert.FromBase64String(b64data); 
+        }
+
+        private static string AppendTrailingChars(string data)
+        {
+            switch (data.Length % 4)
+            {
+                case 0: return data;
+                case 2: return data + "=="; 
+                case 3: return data + "="; 
+                default: throw new ArgumentException(nameof(data));
+            }
+        }
+
+        private static string RemoveTrailingChars(string data)
+        {
+            return data.Split('=')[0];
+        }
+    }
+}
---
+++ src/Util/Contract/ITokenProvider.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ITokenProvider.cs" company="Thomas Stollenwerk (motmot80)">
+// Copyright (c) Thomas Stollenwerk (motmot80). All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Assets.Abune.Client.Unity.Shared.Util.Contract
+{
+    using System;
+
+    /// <summary>
+    /// Interface for token proider
+    /// </summary>
+    public interface ITokenProvider
+    {
+        /// <summary>Creates the JWT token.</summary>
+        /// <param name="authenticationChallenge">The authentication challenge.</param>
+        /// <param name="expires">The expires.</param>
+        /// <returns>Json web token.</returns>
+        string CreateJWTToken(string authenticationChallenge, DateTime expires);
+    }
+}
